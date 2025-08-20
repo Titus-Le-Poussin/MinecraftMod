f37dae2a444f59c7dd48359b1bba6a6ea837ea62
Timothée <timotheeithier@gmail.com>
2025-08-20 16:40:19 +0200

    le mod fonctionne

--- Minecraft_Forge_1.16.5 build/src/main/java/com/Titus_TheDuck/DwarfMod/DwarfMod.java
+++ Minecraft_Forge_1.16.5 build/src/main/java/com/Titus_TheDuck/DwarfMod/DwarfMod.java
@@ -5,10 +5,23 @@ import org.apache.logging.log4j.Logger;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
+import com.Titus_TheDuck.DwarfMod.items.RubisItem;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.fml.RegistryObject;
+import net.minecraft.item.Item;
 
 @Mod("dwarfmod")
 public class DwarfMod {
     public static final String MOD_ID = "dwarfmod";
+    // Registre pour nos items custom
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, MOD_ID);
+
+    // Notre rubis !
+    public static final RegistryObject<Item> RUBIS = ITEMS.register("rubis", RubisItem::new);
+
+
+    
     private static final Logger LOGGER = LogManager.getLogger();
     public DwarfMod() {
         // Enregistrement du bus d'événements
@@ -18,10 +31,12 @@ public class DwarfMod {
         modEventBus.addListener(this::setup);
         
         // Enregistrement des items, blocs, etc. (à ajouter ici)
+        ITEMS.register(modEventBus);
     }
 
     private void setup(final FMLCommonSetupEvent event) {
         LOGGER.info("HELLO FROM DWARF MOD SETUP!");
         LOGGER.info("Le mod Dwarf est maintenant prêt !");
+        LOGGER.info("Rubis enregistré avec succès !");
     }
 }
---
+++ Minecraft_Forge_1.16.5 build/src/main/java/com/Titus_TheDuck/DwarfMod/items/RubisItem.java
@@ -0,0 +1,12 @@
+package com.Titus_TheDuck.DwarfMod.items;
+
+import net.minecraft.item.Item;
+
+public class RubisItem extends Item {
+    public RubisItem() {
+        super(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64));
+    }
+}
+
+
+//./gradlew runClient pour build
