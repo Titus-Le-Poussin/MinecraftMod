78ee302094dd5a42fbddc5f90e52968d629d944a
Timothée <timotheeithier@gmail.com>
2025-08-20 17:12:07 +0200

    Ajout item

--- Minecraft_Forge_1.16.5 build/src/main/java/com/Titus_TheDuck/DwarfMod/items/RubisItem.java
+++ Minecraft_Forge_1.16.5 build/src/main/java/com/Titus_TheDuck/DwarfMod/items/RubisItem.java
@@ -1,10 +1,11 @@
 package com.Titus_TheDuck.DwarfMod.items;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
 
 public class RubisItem extends Item {
     public RubisItem() {
-        super(new Properties().tab(ItemGroup.TAB_MISC).stacksTo(64));
+        super(new Properties().tab(ItemGroup.TAB_MISC));
     }
 }
 
