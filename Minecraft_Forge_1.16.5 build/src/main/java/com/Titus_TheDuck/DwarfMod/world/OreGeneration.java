5e435fd8ef2d2c43514fb1cdebe55aea70f976cc
Timothée <timotheeithier@gmail.com>
2025-08-20 18:44:25 +0200

    le rubis avec génération

--- Minecraft_Forge_1.16.5 build/src/main/java/com/Titus_TheDuck/DwarfMod/world/OreGeneration.java
+++ Minecraft_Forge_1.16.5 build/src/main/java/com/Titus_TheDuck/DwarfMod/world/OreGeneration.java
@@ -6,24 +6,23 @@ import net.minecraft.world.gen.feature.Feature;
 import net.minecraft.world.gen.feature.OreFeatureConfig;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
 import net.minecraft.world.gen.placement.Placement;
-import net.minecraft.world.gen.placement.CountRangeConfig;
+import net.minecraft.world.gen.placement.TopSolidRangeConfig;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
+
 public class OreGeneration {
     
-    // Configuration du minerai de rubis
     public static final ConfiguredFeature<?, ?> RUBIS_ORE_FEATURE = Feature.ORE
         .configured(new OreFeatureConfig(
             OreFeatureConfig.FillerBlockType.NATURAL_STONE,
             DwarfMod.RUBIS_ORE.get().defaultBlockState(),
-            3)) // taille de veine (1-3 blocs)
-        .decorated(Placement.COUNT_RANGE.configured(
-            new CountRangeConfig(6, 5, 0, 20))); // 6 tentatives, Y 5-20
+            15)) // 15 blocs de rubis par veine
+        .decorated(Placement.RANGE.configured(
+    new TopSolidRangeConfig(15, 0, 40))); // Plus de tentatives, zone Y 15-40
 
     @SubscribeEvent
     public static void onBiomeLoading(BiomeLoadingEvent event) {
-        // Ajoute le minerai dans tous les biomes
         event.getGeneration().addFeature(GenerationStage.Decoration.UNDERGROUND_ORES, RUBIS_ORE_FEATURE);
     }
 }
